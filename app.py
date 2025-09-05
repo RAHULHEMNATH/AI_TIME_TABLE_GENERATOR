2b0f928160ad9a61d923471202a27fc2dacce9e4
HEMNATH R <hemnath2892005@gmail.com>
2025-09-05 14:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template # Make sure to import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World! My Flask App is working on Vercel!"
+    # Try to render your main homepage template
+    # Replace 'index.html' with the name of your actual main template file
+    return render_template('main.py')
 
-# This is very important for running it on your computer
 if __name__ == '__main__':
     app.run(debug=True)
