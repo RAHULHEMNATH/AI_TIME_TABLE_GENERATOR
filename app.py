54490a3c386c5cd798e5ba2287e14d0a7b0240bf
HEMNATH R <hemnath2892005@gmail.com>
2025-09-05 14:01:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,3 +43,15 @@ else:
 
 # This allows the app to work both locally and on Vercel
 app.vercel_handler = vercel_handler
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World! My Flask App is working on Vercel!"
+
+# This is very important for running it on your computer
+if __name__ == '__main__':
+    app.run(debug=True)
